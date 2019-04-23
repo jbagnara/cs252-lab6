8c293bc76e506d996a68646fe2cb2884387b0685
Kurtis Dressel <kurtisdoubled@hotmail.com>
2019-04-22 21:11:23 -0400

    added row removal (not tested)

--- Tetris/Main/tetris.py
+++ Tetris/Main/tetris.py
@@ -6,9 +6,10 @@ class Tetris():
         self.height = 20
 
         self.field = [[CellState.EMPTY for y in range(self.height)]
-                      for x in range(self.width)]
+                for x in range(self.width)]
 
         self.num_players = 0
+        self.score = 0
         
         self.curr_piece = Piece()  # get random tetromino
         self.draw_piece()
@@ -48,3 +49,45 @@ class Tetris():
             for y in range(self.curr_piece.y, self.curr_piece.y + len(piece)):
                 if x < self.width and x >= 0 and y < self.height and y >= 0:
                     self.field[x][y] = CellState.EMPTY
+
+    def is_row_full(self, row):
+        for x in range(self.width):
+            if self.field[x][row] == CellState.EMPTY:
+                return True
+
+        return False
+
+    def clear_rows(self):
+        #check for full rows
+        num_full_rows = 0
+        for y in range(self.height-1, 0, -1):
+            if self.is_row_full(y):
+                num_full_rows += 1
+
+        #update score
+        if num_full_rows == 1: #single
+            self.score += 40
+        elif num_full_rows == 2: #double
+            self.score += 100
+        elif num_full_rows == 3: #triple
+            self.score += 300
+        elif num_full_rows >= 4: #tetris
+            self.score += 1200
+
+        #update field
+        for y in range(self.height-num_full_rows, self.height):
+            for x in range(self.width):
+                self.field[x][y] = CellState.EMPTY
+
+        return num_full_rows
+
+    def move_rows_down(self, num_full_rows):
+        #move rows down
+        for y in range(self.height-1, 0, -1):
+            for x in range(self.width):
+                self.field[x][y] = self.field[x][y+num_full_rows]
+            
+        #add new rows at top
+        for y in range(num_full_rows):
+            for x in range(self.width):
+                self.field[x][y] = CellState.EMPTY
