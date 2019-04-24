2dc8b7d3b95421f94b5a60475b77c3c4e40969dc
Peter Huang <pete_huang88@yahoo.com>
2019-04-24 13:18:29 -0700

    asdf

--- Tetris/settings.py
+++ Tetris/settings.py
@@ -143,7 +143,7 @@ CHANNEL_LAYERS = {
 	"default": {
 		"BACKEND": "channels_redis.core.RedisChannelLayer",
 		"CONFIG": {
-			"hosts": [("127.0.0.1", 6379)],
+			"hosts": [("127.0.0.1", 6379), os.environ.get('REDIS_URL', 'redis://localhost:6379')],
 		},
 	},
 }
