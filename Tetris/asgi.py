c98ba10d6f37e029e14b9b1b471198307d3b89ac
Peter Huang <pete_huang88@yahoo.com>
2019-04-24 14:06:01 -0700

    lol

--- Tetris/asgi.py
+++ Tetris/asgi.py
@@ -1,8 +1,10 @@
 
 import os
+import django
 
-from channels.asgi import get_channel_layer
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tetris.settings')
+django.setup()
 
-channel_layer = get_channel_layer()
+application = get_default_application()
